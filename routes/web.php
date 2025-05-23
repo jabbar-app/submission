dbf0e4ca0e3c6d7f3cbe92727e584f09968a0829
Jab <jabbarpanggabean@gmail.com>
2025-05-23 18:19:45 +0700

    fix: update Google login

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@ Route::get('/login', function () {
 Route::get('/auth/google', [GoogleAuthController::class, 'redirectToGoogle'])->name('auth.google');
 Route::get('/auth/google/callback', [GoogleAuthController::class, 'handleGoogleCallback']);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/', [ProjectController::class, 'create'])->name('project.create');
-    Route::post('/project/submit', [ProjectController::class, 'store'])->name('project.store');
-    Route::get('/project/{project}/confirm-fee', [ProjectController::class, 'confirmFee'])->name('project.confirmFee');
-    Route::get('/project/{project}/success', [ProjectController::class, 'success'])->name('project.success');
-});
+Route::get('/', [ProjectController::class, 'create'])->name('project.create');
+Route::post('/project/submit', [ProjectController::class, 'store'])->name('project.store');
+Route::get('/project/{project}/confirm-fee', [ProjectController::class, 'confirmFee'])->name('project.confirmFee');
+Route::get('/project/{project}/success', [ProjectController::class, 'success'])->name('project.success');
+
+Route::middleware('auth')->group(function () {});
